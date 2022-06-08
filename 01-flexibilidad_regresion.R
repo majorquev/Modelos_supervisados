84fa39aa749e1981183480b1e88a9b688351f0d3
Miguel Jorquera Viguera <32852412+majorquev@users.noreply.github.com>
2022-06-08 04:40:16 -0400

    Update 01-flexibilidad_regresion.R

--- 01-flexibilidad_regresion.R
+++ 01-flexibilidad_regresion.R
@@ -1,93 +1,143 @@
 library(ggplot2)
-library(class)
-x<-seq(0,4,by=0.01)
+library(dplyr)
 
+set.seed(99)
+x <- rnorm(100,sd = 1)
+e <- rnorm(100,sd = 3)
 
-f<-function(x){
-  t<-(-x-1)*(x-3)*(x-4)
-return(t)
-  }
-
-plot(x,f(x),type="l")
-
-
-set.seed(123)
-x1<-runif(100,0,4)
-y1<-f(x1)+ rnorm(100,sd=1.5)
+y <- 1 + x^3  +e
 
 
 
 
-train <- data.frame(
-  x1 = x1,
-  y1 = y1,
-  x2 = x1 ^ 2,
-  x3 = x1 ^ 3,
-  x4 = x1 ^ 4,
-  x5 = x1 ^ 5,
-  x6 = x1 ^ 6,
-  x7 = x1 ^ 7,
-  x8 = x1 ^ 8,
-  x9 = x1 ^ 9,
-  x10 = x1 ^ 10,
-  x11 = x1 ^ 11
+x2 <- x^2
+x3 <- x^3
+x4 <- x^4
+x5 <- x^5
+x6 <- x^6
+x7 <- x^7
+x8 <- x^8
+x9 <- x^9
+x10 <- x^10
+x11 <- x^11
+
+
+df <- data.frame(x = x,
+                 y= y,
+                 x2 = x2 ,
+                 x3 = x3 ,
+                 x4 = x4 ,
+                 x5 = x5 ,
+                 x6 = x6 ,
+                 x7 = x7 ,
+                 x8 = x8 ,
+                 x9 = x9 ,
+                 x10= x10,
+                 x11= x11
+                 
 )
 
+train <- sample_frac(df,.7)
+test <- anti_join(df,train)
+
+ggplot(df) +
+  aes(x,y) +
+  geom_point()
+
+ggplot(train) +
+  aes(x,y) +
+  geom_point(col = "red") +
+  geom_point(mapping = aes(x,y),data = test, color ="green")
 
 
-plot(train$x1,train$y1)
+m1 <- lm(y ~ x, data = train)
+m2 <- lm(y ~ x + x2, data = train)
+m3 <- lm(y ~ x + x2 + x3, data = train)
+m4 <- lm(y ~ x + x2 + x3 + x4 , data = train)
+m5 <- lm(y ~ x + x2 + x3 + x4 + x5, data = train)
+m6 <- lm(y ~ x + x2 + x3 + x4 + x5 +x6 + x7 + x8+ x9, data = train)
+m7 <- lm(y ~ x + x2 + x3 + x4 + x5 +x6 + x7 + x8+ x9 + x10 + x11, data = train)
+mkn1 <- knnreg(train["x"], train$y, k = 1)
+
+results <- data.frame(x=rep(train$x,6),
+                      y=rep(train$y,6),
+                      pred_train = c(predict(m1),
+                                     predict(m2),
+                                     predict(m3),
+                                     predict(m6),
+                                     predict(m7), 
+                                     predict(mkn1,train["x"]) ),
+                      model = c(rep("m1",70),
+                                rep("m2",70),
+                                rep("m3",70),
+                                rep("m6",70),
+                                rep("m7",70),
+                                rep("kn1",70)))
+
+ggplot(results) +
+  geom_point(aes(x = x , y =y)) +
+  geom_line(aes(x = x, y = pred_train,col = model)) + 
+  facet_wrap(~ model)
 
 
-x1t<-runif(50,0,4)
-test<-data.frame(
-  x1 = x1t,
-  y1 = f(x1t),
-  x2 = x1t ^ 2,
-  x3 = x1t ^ 3,
-  x4 = x1t ^ 4,
-  x5 = x1t ^ 5,
-  x6 = x1t ^ 6,
-  x7 = x1t ^ 7,
-  x8 = x1t ^ 8,
-  x9 = x1t ^ 9,
-  x10 = x1t ^ 10,
-  x11 = x1t ^ 11
-  
-)
+# Generamos predicciones y calculamos el errorcuadrático medio en  --------
+
+p1_test <- predict(m1, test)
+p2_test <- predict(m2, test)
+p3_test <- predict(m3, test)
+p6_test <- predict(m6, test)
+p7_test <- predict(m7, test)
+pkn1_test <- predict(mkn1,test["x"]) 
+
+p1_train <- predict(m1, train)
+p2_train <- predict(m2, train)
+p3_train <- predict(m3, train)
+p6_train <- predict(m6, train)
+p7_train <- predict(m7, train)
+pkn1_train <- predict(mkn1,train["x"])
 
 
-mod1<-lm(y1~x1,data=train)
-mod2<-lm(y1~x1+x2+x3,data=train)
-mod3<-lm(y1~x1+x2+x3+x4+x5,data=train)
-mod4<-lm(y1~x1+x2+x3+x4+x5+x6+x7,data=train)
-mod5<-lm(y1~x1+x2+x3+x4+x5+x6+x7+x8+x9,data=train)
-mod6<-lm(y1~x1+x2+x3+x4+x5+x6+x7+x8+x9+x10+x11,data=train)
+# MAE (error absoluto medio)
+mean(abs(p1_test - test$y) )
+mean(abs(p1_train - train$y) )
+
+# MSE (error cuadrático medio)
+mean((p1_test - test$y)^2)
+mean((p1_train - train$y)^2)
+
+# RMSE (root mean square error)(raiz del error cuadrático medio)
+sqrt(mean((p1_test - test$y)^2))
+sqrt(mean((p1_train - train$y)^2))
 
 
-(e1<-sqrt(sum((train$y1-predict(mod1))^2)/(dim(train)[1])))
-(e2<-sqrt(sum((train$y1-predict(mod2))^2)/(dim(train)[1])))
-(e3<-sqrt(sum((train$y1-predict(mod3))^2)/(dim(train)[1])))
-(e4<-sqrt(sum((train$y1-predict(mod4))^2)/(dim(train)[1])))
-(e5<-sqrt(sum((train$y1-predict(mod5))^2)/(dim(train)[1])))
-(e6<-sqrt(sum((train$y1-predict(mod6))^2)/(dim(train)[1])))
+sqrt(mean((pkn1_test - test$y)^2))
+sqrt(mean((pkn1_train - train$y)^2))
 
+# Errores de test y train -------------------------------------------------
 
-ggplot(train) + geom_point(aes(x1,y1))+geom_line(aes(x1,predict(mod1) ),color="red")+
-  geom_line(aes(x1,predict(mod2) ),color="blue")+
-  geom_line(aes(x1,predict(mod6) ),color="green")
+# MSE train 
+mse_train <- c(
+  mean((p1_train - train$y)^2),
+  mean((p2_train - train$y)^2),
+  mean((p3_train - train$y)^2),
+  mean((p6_train - train$y)^2),
+  mean((p7_train - train$y)^2),
+  mean((pkn1_train - train$y)^2)
+  )
 
+# MSE test
+mse_test <- c(mean((p1_test - test$y)^2),
+              mean((p2_test - test$y)^2),
+              mean((p3_test - test$y)^2),
+              mean((p6_test - test$y)^2),
+              mean((p7_test - test$y)^2),
+              mean((pkn1_test - test$y)^2)
+              )
 
-(e1t<-sqrt(sum((test$y1-predict(mod1,newdata = test))^2)/(dim(test)[1])))
-(e2t<-sqrt(sum((test$y1-predict(mod2,newdata = test))^2)/(dim(test)[1])))
-(e3t<-sqrt(sum((test$y1-predict(mod3,newdata = test))^2)/(dim(test)[1])))
-(e4t<-sqrt(sum((test$y1-predict(mod4,newdata = test))^2)/(dim(test)[1])))
-(e5t<-sqrt(sum((test$y1-predict(mod5,newdata = test))^2)/(dim(test)[1])))
-(e6t<-sqrt(sum((test$y1-predict(mod6,newdata = test))^2)/(dim(test)[1])))
-
-
-err<-data.frame(p=c(1,3,5,7,9,11),e=c(e1,e2,e3,e4,e5,e6),et=c(e1t,e2t,e3t,e4t,e5t,e6t))
-
-ggplot(err) + geom_line(aes(p,e),color="red") + geom_line(aes(p,et),color="blue")
-
-
-
+errores <- data.frame(mse_tain = mse_train,
+           mse_test = mse_test,
+           modelo = c(1,2,3,6,7,Inf)) 
+errores %>% 
+  ggplot() +
+  geom_line(aes(x = modelo, y = mse_test),col = "red" ) +
+  geom_line(aes(x = modelo, y = mse_train),col = "green" ) 
